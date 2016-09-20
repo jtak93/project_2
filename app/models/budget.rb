ee85a6411bf4c8d2a579f8731e749652c8012901
james tak <jtak93@gmail.com>
2016-09-20 11:40:40 -0700

    adds expense links and amount validation for budget and expense model

--- app/models/budget.rb
+++ app/models/budget.rb
@@ -1,6 +1,7 @@
 class Budget < ActiveRecord::Base
   belongs_to :user
   has_many :expenses, dependent: :destroy
+  validates :budget, presence: true
 
   after_initialize :set_defaults, unless: :persisted?
   # The set_defaults will only work if the object is new
